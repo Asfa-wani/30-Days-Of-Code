21336e2c74ff80b522f07c44e7d2c40b2fd134ab
Asfa Wani <waniasfa@gmail.com>
2022-08-01 17:45:00 +0530

    connected to db

--- Day-one-of-code/server.js
+++ Day-one-of-code/server.js
@@ -5,13 +5,28 @@ var corsOptions = {
     origin: "http://localhost:8081"
 }
 
+const db = require("./models/index");
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
 app.use(cors(corsOptions));
+// parse requests of content-type - application/json
 app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-    res.json({ message: "Hello this is a test" })
-})
-
+        res.json({ message: "Hello this is a test" })
+    })
+    // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`server started at port ${PORT}`);

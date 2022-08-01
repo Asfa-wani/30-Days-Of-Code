c329afc0a12b45cf095fa9d0932bd83101be7dc2
Asfa Wani <waniasfa@gmail.com>
2022-08-01 21:18:58 +0530

    added update function

--- Day-one-of-code/constrollers/tutorials.controller.js
+++ Day-one-of-code/constrollers/tutorials.controller.js
@@ -6,7 +6,7 @@ exports.create = (req, res) => {
 
     //validte
     if (!req.body)
-        res.status(400).send({ message: "Cannot be empty" });
+        return res.status(400).send({ message: "Cannot be empty" });
     const tutorials = new Tutorials({
             title: req.body.title,
             description: req.body.description,
@@ -49,7 +49,25 @@ exports.findOne = (req, res) => {
 };
 // Update a Tutorial by the id in the request
 exports.update = (req, res) => {
-
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }
+    const id = req.params.id;
+    Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+                });
+            } else res.send({ message: "Tutorial was updated successfully." });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Tutorial with id=" + id
+            });
+        });
 };
 // Delete a Tutorial with the specified id in the request
 exports.delete = (req, res) => {

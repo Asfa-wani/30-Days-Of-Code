bb399977d61d85a8e6d4a2dd9161d1c0fbbdf8a2
Asfa Wani <waniasfa@gmail.com>
2022-08-02 15:21:51 +0530

    set up the simple server

---
+++ Day-two-of-code/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({ message: "This is second day of code practce" });
+})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+})

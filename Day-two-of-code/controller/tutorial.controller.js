da8e04434deadddbbc1c187f17ef51cb8dd62c31
Asfa Wani <waniasfa@gmail.com>
2022-08-02 16:00:55 +0530

    added find by id function

--- Day-two-of-code/controller/tutorial.controller.js
+++ Day-two-of-code/controller/tutorial.controller.js
@@ -20,8 +20,11 @@ exports.create = (req, res) => {
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
     const title = req.query.title;
-    var condition = title ? { title: {
-            [Op.iLike]: `%${title}%` } } : null;
+    var condition = title ? {
+        title: {
+            [Op.iLike]: `%${title}%`
+        }
+    } : null;
     Tutorial.findAll({ where: condition })
         .then(data => {
             if (!data)
@@ -34,8 +37,24 @@ exports.findAll = (req, res) => {
 };
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
-
+    const id = req.params.id;
+    Tutorial.findByPk(id)
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `Cannot find Tutorial with id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Tutorial with id=" + id
+            });
+        });
 };
+
 // Update a Tutorial by the id in the request
 exports.update = (req, res) => {
 

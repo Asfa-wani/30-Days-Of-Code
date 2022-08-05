c62b06f7c681cc8374d1f041aeb8a2b8c2336be3
Asfa Wani <waniasfa@gmail.com>
2022-08-05 15:25:25 +0530

    created server functions

--- Day-5-of-code/src/models/Identifier.js
+++ Day-5-of-code/src/models/Identifier.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
-
 const Identifier = mongoose.model(
-    Identifier, new mongoose.model({
-        carCode: String,
-        customer: { type: mongoose.Schema.Types.ObjectId, ref: "Customer" },
+    "Identifier",
+    new mongoose.Schema({
+        cardCode: String,
+        customer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Customer"
+        }
     })
 );
 module.exports = Identifier;

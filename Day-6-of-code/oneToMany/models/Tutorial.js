84bf65ac70a98e165baf805df5321d58f9acf9e8
Asfa Wani <waniasfa@gmail.com>
2022-08-06 17:37:06 +0530

    created tutorial model

---
+++ Day-6-of-code/oneToMany/models/Tutorial.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Tutorial = mongoose.model(
+    "Tutorial",
+    new mongoose.Schema({
+        title: String,
+        author: String,
+        images: []
+    })
+);
+module.exports = Tutorial;

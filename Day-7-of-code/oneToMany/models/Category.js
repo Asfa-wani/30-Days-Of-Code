3bd3a341d32ea03496b5836b917be18a34f49a90
Asfa Wani <waniasfa@gmail.com>
2022-08-07 20:54:33 +0530

    added category schema

---
+++ Day-7-of-code/oneToMany/models/Category.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Category = mongoose.model(
+    "Category",
+    new mongoose.Schema({
+        name: String,
+        description: String
+    })
+);
+module.exports = Category;

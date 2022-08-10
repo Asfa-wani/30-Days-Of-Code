1ed4d2a865dc7104667a2948c8d466e5398a201b
Asfa Wani <waniasfa@gmail.com>
2022-08-10 23:02:13 +0530

    created comment

--- Day-10-of-code/server.js
+++ Day-10-of-code/server.js
@@ -1,9 +1,19 @@
-const db = require("./app/models");
+const db = require("./models");
 const controller = require("./controllers/tutorial.controller");
 const run = async() => {
     const tut1 = await controller.createTutorial({
         title: "Tut#1",
         description: "Tut#1 Description",
+    });
+
+    const tut2 = await controller.createTutorial({
+        title: "Tut#2",
+        description: "Tut#2 Description",
+    });
+
+    const comment1 = await controller.createComment(tut1.id, {
+        name: "bezkoder",
+        text: "Good job!",
     });
 };
 // db.sequelize.sync();

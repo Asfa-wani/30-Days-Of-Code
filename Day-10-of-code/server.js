458696f55147b2010cc47a828950a2cd815e4b32
Asfa Wani <waniasfa@gmail.com>
2022-08-10 23:00:59 +0530

    created tutorial

--- Day-10-of-code/server.js
+++ Day-10-of-code/server.js
@@ -1,6 +1,11 @@
 const db = require("./app/models");
 const controller = require("./controllers/tutorial.controller");
-const run = async() => {};
+const run = async() => {
+    const tut1 = await controller.createTutorial({
+        title: "Tut#1",
+        description: "Tut#1 Description",
+    });
+};
 // db.sequelize.sync();
 db.sequelize.sync({ force: true }).then(() => {
     console.log("Drop and re-sync db.");

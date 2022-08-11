182036f6a078c65865d1345d20948de20c2df491
Asfa Wani <waniasfa@gmail.com>
2022-08-11 23:25:33 +0530

    added tutorials

--- Day-11-of-code/server.js
+++ Day-11-of-code/server.js
@@ -1,7 +1,26 @@
 const db = require("./models");
 const TutorialController = require("./controllers/tutorial.controller");
 const TagController = require("./controllers/tag.controller");
-const run = async() => {};
+const run = async() => {
+
+    const tut1 = await TutorialController.create({
+        title: "Tut#1",
+        description: "Tut#1 Description",
+    });
+
+    const tut2 = await TutorialController.create({
+        title: "Tut#2",
+        description: "Tut#2 Description",
+    });
+    const tut3 = await TutorialController.create({
+        title: "Tut#3",
+        description: "Tut#3 Description",
+    });
+    const tut4 = await TutorialController.create({
+        title: "Tut#4",
+        description: "Tut#4 Description",
+    });
+};
 // db.sequelize.sync();
 db.sequelize.sync({ force: true }).then(() => {
     console.log("Drop and re-sync db.");

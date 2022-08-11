db9e2d7196f7a90d3466fafd0617a40733b2c64e
Asfa Wani <waniasfa@gmail.com>
2022-08-11 23:22:56 +0530

    added tutiorial functions

--- Day-11-of-code/controllers/tutorial.controller.js
+++ Day-11-of-code/controllers/tutorial.controller.js
@@ -2,75 +2,61 @@ const db = require("../models");
 const Tutorial = db.tutorial;
 const Tag = db.tag;
 
-exports.create = (tag) => {
-    return Tag.create({
-            name: tag.name,
+exports.create = (tutorial) => {
+    return Tutorial.create({
+            title: tutorial.title,
+            description: tutorial.description,
         })
-        .then((tag) => {
-            console.log(">> Created Tag: " + JSON.stringify(tag, null, 2));
-            return tag;
+        .then((tutorial) => {
+            console.log(">> Created Tutorial: " + JSON.stringify(tutorial, null, 4));
+            return tutorial;
         })
         .catch((err) => {
-            console.log(">> Error while creating Tag: ", err);
+            console.log(">> Error while creating Tutorial: ", err);
         });
 };
 
 exports.findAll = () => {
-    return Tag.findAll({
+    return Tutorial.findAll({
             include: [{
-                model: Tutorial,
-                as: "tutorials",
-                attributes: ["id", "title", "description"],
+                model: Tag,
+                as: "tags",
+                attributes: ["id", "name"],
                 through: {
                     attributes: [],
-                }
+                },
+                // through: {
+                //   attributes: ["tag_id", "tutorial_id"],
+                // },
             }, ],
         })
-        .then((tags) => {
-            return tags;
+        .then((tutorials) => {
+            return tutorials;
         })
         .catch((err) => {
-            console.log(">> Error while retrieving Tags: ", err);
+            console.log(">> Error while retrieving Tutorials: ", err);
         });
 };
+
 
 exports.findById = (id) => {
-    return Tag.findByPk(id, {
+    return Tutorial.findByPk(id, {
             include: [{
-                model: Tutorial,
-                as: "tutorials",
-                attributes: ["id", "title", "description"],
+                model: Tag,
+                as: "tags",
+                attributes: ["id", "name"],
                 through: {
                     attributes: [],
-                }
+                },
+                // through: {
+                //   attributes: ["tag_id", "tutorial_id"],
+                // },
             }, ],
         })
-        .then((tag) => {
-            return tag;
+        .then((tutorial) => {
+            return tutorial;
         })
         .catch((err) => {
-            console.log(">> Error while finding Tag: ", err);
-        });
-};
-
-exports.addTutorial = (tagId, tutorialId) => {
-    return Tag.findByPk(tagId)
-        .then((tag) => {
-            if (!tag) {
-                console.log("Tag not found!");
-                return null;
-            }
-            return Tutorial.findByPk(tutorialId).then((tutorial) => {
-                if (!tutorial) {
-                    console.log("Tutorial not found!");
-                    return null;
-                }
-                tag.addTutorial(tutorial);
-                console.log(`>> added Tutorial id=${tutorial.id} to Tag id=${tag.id}`);
-                return tag;
-            });
-        })
-        .catch((err) => {
-            console.log(">> Error while adding Tutorial to Tag: ", err);
+            console.log(">> Error while finding Tutorial: ", err);
         });
 };

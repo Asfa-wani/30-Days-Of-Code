983a82a1fd7df063ea39c55532fb2715d4c01c78
Asfa Wani <waniasfa@gmail.com>
2022-08-13 20:00:13 +0530

    modified middlewares

--- Day-13-of-code/src/middleware/upload.js
+++ Day-13-of-code/src/middleware/upload.js
@@ -1,20 +1,58 @@
-const util = require("util");
-const path = require("path");
 const multer = require("multer");
-var storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, path.join(`${__dirname}/../../upload`));
-    },
-    filename: (req, file, callback) => {
-        const match = ["image/png", "image/jpeg"];
-        if (match.indexOf(file.mimetype) === -1) {
-            var message = `${file.originalname} is invalid. Only accept png/jpeg.`;
-            return callback(message, null);
-        }
-        var filename = `${Date.now()}-bezkoder-${file.originalname}`;
-        callback(null, filename);
+const sharp = require("sharp");
+const multerStorage = multer.memoryStorage();
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith("image")) {
+        cb(null, true);
+    } else {
+        cb("Please upload only images.", false);
     }
+};
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter
 });
-var uploadFiles = multer({ storage: storage }).array("multi-files", 10);
-var uploadFilesMiddleware = util.promisify(uploadFiles);
-module.exports = uploadFilesMiddleware;
+const uploadFiles = upload.array("images", 10);
+const uploadImages = (req, res, next) => {
+    uploadFiles(req, res, err => {
+        if (err instanceof multer.MulterError) {
+            if (err.code === "LIMIT_UNEXPECTED_FILE") {
+                return res.send("Too many files to upload.");
+            }
+        } else if (err) {
+            return res.send(err);
+        }
+        next();
+    });
+};
+const resizeImages = async(req, res, next) => {
+    if (!req.files) return next();
+    req.body.images = [];
+    await Promise.all(
+        req.files.map(async file => {
+            const filename = file.originalname.replace(/\..+$/, "");
+            const newFilename = `bezkoder-${filename}-${Date.now()}.jpeg`;
+            await sharp(file.buffer)
+                .resize(640, 320)
+                .toFormat("jpeg")
+                .jpeg({ quality: 90 })
+                .toFile(`upload/${newFilename}`);
+            req.body.images.push(newFilename);
+        })
+    );
+    next();
+};
+const getResult = async(req, res) => {
+    if (req.body.images.length <= 0) {
+        return res.send(`You must select at least 1 image.`);
+    }
+    const images = req.body.images
+        .map(image => "" + image + "")
+        .join("");
+    return res.send(`Images were uploaded:${images}`);
+};
+module.exports = {
+    uploadImages: uploadImages,
+    resizeImages: resizeImages,
+    getResult: getResult
+};

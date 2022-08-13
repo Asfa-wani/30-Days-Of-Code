556f3ec5075a1fab861636bc3b48195a7dc53349
Asfa Wani <waniasfa@gmail.com>
2022-08-13 20:05:22 +0530

    modified

--- Day-13-of-code/src/routes/web.js
+++ Day-13-of-code/src/routes/web.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home");
-const uploadController = require("./../controllers/upload");
+const uploadController = require("../controllers/upload");
 let routes = app => {
     router.get("/", homeController.getHome);
     router.post(

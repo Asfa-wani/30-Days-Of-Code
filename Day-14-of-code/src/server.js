8f25bb2395558eda5b8073648663ba495162c9ea
Asfa Wani <waniasfa@gmail.com>
2022-08-14 20:42:46 +0530

    modified

--- Day-14-of-code/src/server.js
+++ Day-14-of-code/src/server.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const initRoutes = require("./routes");
+const initRoutes = require("./routes/web");
 var corsOptions = {
     origin: "http://localhost:8081"
 };

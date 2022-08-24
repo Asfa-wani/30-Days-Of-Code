f4c63637e864c65e5ab51b346f09f57bc0861450
Asfa Wani <waniasfa@gmail.com>
2022-08-24 21:49:49 +0530

    created user api

--- Day-21-of-code/server.js
+++ Day-21-of-code/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-
+const userRoute = require("./server/routes/user")
 const corsOptions = {
     origin: "http://localhost:8081"
 };
 const connection = require("./server/config/db.config");
 const app = express();
 connection();
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/user", userRoute);
 
 
 const PORT = process.env.PORT || 8080;

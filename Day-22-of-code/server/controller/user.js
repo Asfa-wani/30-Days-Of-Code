7a3a7f42240b32346badae47d7a3ec9ff3c8c428
Asfa Wani <waniasfa@gmail.com>
2022-08-25 11:23:00 +0530

    created user controller

---
+++ Day-22-of-code/server/controller/user.js
@@ -0,0 +1,11 @@
+const { User, validate } = require("../models/user");
+
+
+const createUser = async(req, res) => {
+    try {
+
+        const { name, username, email, gender, DOB } = req.body;
+    } catch (error) {
+
+    }
+}

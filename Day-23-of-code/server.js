0c2f66ba5658be2e98d7707ece518be5c78abd6c
Asfa Wani <waniasfa@gmail.com>
2022-08-26 20:42:52 +0530

    modified

--- Day-23-of-code/server.js
+++ Day-23-of-code/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const userRoute = require("./server/routes/user")
+const userRoute = require("./server/routes/user");
+const path = require("path");
 const corsOptions = {
     origin: "http://localhost:8081"
 };
 const connection = require("./server/config/db.config");
 const app = express();
+const static_path = path.join(__dirname, "./server/view");
+//console.log(path.join(__dirname, "./server/view"))
 connection();
+app.use(express.static(static_path));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

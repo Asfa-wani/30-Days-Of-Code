fa855f30a062004724464a09440cf178661f41dd
Asfa Wani <waniasfa@gmail.com>
2022-08-26 20:08:56 +0530

    created the basic structure

---
+++ Day-23-of-code/server/controller/user.js
@@ -0,0 +1,21 @@
+const { User, validate } = require("../models/user");
+
+
+const createUser = async(req, res) => {
+    try {
+
+        const { name, username, email, gender, DOB } = req.body;
+        const { error } = validate({ name, username, email, gender, DOB });
+        if (error)
+            return res.status(403).send({ message: error.details[0].message });
+        const user = await User.findOne({ email: email });
+        if (user)
+            return res.status(409).send({ message: "User with this email already exists" });
+        await new User({ name, username, email, gender, DOB }).save();
+        res.status(200).send({ message: "User registered successfully!" });
+    } catch (error) {
+        res.status(500).send({ message: "Server error" });
+    }
+};
+
+module.exports = { createUser };
---
+++ Day-23-of-code/server/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+//const jwt = require("jsonwebtoken");
+const joi = require("joi");
+
+const userSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    gender: { type: String, required: true },
+    DOB: { type: Date }
+});
+
+
+
+
+/* userSchema.methods.genToken = function() {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SEC, { expiresIn: "7days" });
+    return token;
+}; */
+
+const User = mongoose.model("User", userSchema);
+
+const validate = (data) => {
+    const schema = joi.object({
+        name: joi.string().required().label("Name"),
+        username: joi.string().required().unique().label("Username"),
+        email: joi.email().required().label("Email")
+    });
+    return schema.validate(data);
+};
+
+module.exports = { User, validate };
